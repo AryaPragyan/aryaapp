587d9a1f4bcb2106ee8f48c9b8f7c1f211974919
arya <arya@ubuntu.ubuntu-domain>
2012-11-08 11:34:24 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aryaapp::Application.config.secret_token = '018e1a6ec9f22444a7bc7eb4a93cd3c9c7e478ed3ab702ea5359e18132a00b364c51e9a1a30ae0249615e098e67d4423d3d4bfecc8a21ea24e00508025b58533'
